488c48f3439aabb14c58f47179f9d2d2978c4ff2
riadhassan <riad_hassan@outlook.com>
2025-07-29 17:10:12 +0000

    Deploying to gh-pages from @ riadhassan/riadhassan.github.io@fa956d1ae04db6cb8045f16b8219ca1688426cd9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -84,51 +84,11 @@ ninja.data = [{
           section: "News",},{id: "news-successfully-completed-msc-from-buet-thesis-uncertainty-driven-boundary-refined-cnn-for-medical-image-segmentation-graduation-cap",
           title: 'Successfully completed MSc from BUET! Thesis: Uncertainty driven boundary refined CNN for medical...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-efficient-multi-organ-segmentation",
+          title: 'Efficient Multi-Organ Segmentation',
+          description: "A deep learning framework for efficient and accurate organ segmentation.",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/Efficient_Organ_Segmentation/";
             },},{
         id: 'social-email',
         title: 'email',
